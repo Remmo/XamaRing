cc3b5505a2c2780b1b0408e120e351843a52d129
Remmo <aporia82@hotmail.com>
2015-02-06 08:57:10 +0100

    Cleaning Solution

--- Utility.Droid/Properties/AssemblyInfo.cs
+++
@@ -1,6 +0,0 @@
-﻿using System.Reflection;
-
-[assembly: AssemblyTitle("XamaRing.Android.Utility")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
